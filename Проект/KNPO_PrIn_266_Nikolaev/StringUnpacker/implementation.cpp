0da7cc65738069791a4639000e9ac6c2512a3491
PRIN366NikolaevArtem <96787026+PRIN366NikolaevArtem@users.noreply.github.com>
2021-11-21 18:01:42 +0300

    Реализация функций
    Реализация двух новых функций:  DivideStringToChars, FillUnitArray

--- Проект/KNPO_PrIn_266_Nikolaev/StringUnpacker/implementation.cpp
+++ Проект/KNPO_PrIn_266_Nikolaev/StringUnpacker/implementation.cpp
@@ -27,6 +27,70 @@
 
 
 /*! 
+    \brief Сформировать модуль и записать его в массив
+    \param[in|out] currentIterator ссылка на текущий символ, на котором остановился итератор
+    \param[in|out] inputData ссылка на текущий символ, на котором остановился итератор
+    \param[in|out] arrayOfCharacters ссылка на текущий символ, на котором остановился итератор
+
+    \return текущий символ, на котором остановился итератор
+*/
+auto FillUnitArray(std::string_view::const_iterator &currentIterator, const std::string_view& inputData, std::vector<Unit> &arrayOfCharacters)
+{
+    /// Начало числа повторений – текущий символ
+    const auto beginIt = currentIterator;
+
+    /// Найти первый нецифровой символ – это запоследний символ числа повторений, а также повторяемая буква
+    const auto endIt = std::find_if(currentIterator, inputData.end(), std::not_fn(std::isdigit));
+
+    /// Конвертировать строку с числом повторением в число
+    const std::string_view currentStr = inputData.substr(beginIt - inputData.begin(), endIt - inputData.begin());
+    int quantity = 0;
+    std::from_chars(currentStr.data(), currentStr.data() + currentStr.size(), quantity);
+
+    /// Если число повторений не входит в разрешенный диапазон – выдать ошибку
+    if (quantity < 2 || quantity > 99)
+        throw ExceptionError{ TermError::WrongRepeat };
+
+    /// Добавить в массив модуль с вычисленным числом повторений и повторяемой буквой
+    arrayOfCharacters.push_back({ quantity, *endIt });
+
+    /// Считать текущим символом следующий символ после повторяемой буквы
+    return endIt;
+}
+
+/*! 
+    \brief Разделить строку на модули
+    \param[in] inputData входная строка
+
+    \return массив модулей
+*/
+std::vector<Unit> DivideStringToChars(const std::string_view inputData)
+{
+    /// Создать массив, в который будем сохранять модули
+    std::vector<Unit> arrayOfCharacters;
+
+    /// Для каждого символа строки
+    for (auto currentIterator = inputData.begin(); currentIterator != inputData.end(); ++currentIterator)
+    {
+        /// Если текущий символ – буква
+        if (std::isalpha(*currentIterator))
+        {
+            /// Добавить в массив модуль с текущим символом и количеством символов равным 1
+            arrayOfCharacters.push_back({1, *currentIterator});
+
+            /// Перейти к следующей итерации
+            continue;
+        }
+        
+        /// Сформировать модуль и записать его в массив
+        currentIterator = FillUnitArray(currentIterator, inputData, arrayOfCharacters);
+    }
+
+    /// Вернуть получившийся массив модулей
+    return arrayOfCharacters;
+}
+
+/*! 
     \brief Проверить содержимое строки на корректность
     \param[in] inputData входная строка
 
