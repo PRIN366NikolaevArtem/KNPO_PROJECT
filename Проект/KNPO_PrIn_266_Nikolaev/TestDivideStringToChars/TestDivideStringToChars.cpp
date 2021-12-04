6caaa0b2cb1bb785a8e4e54ec4c46bfbafa79929
PRIN366NikolaevArtem <96787026+PRIN366NikolaevArtem@users.noreply.github.com>
2021-12-04 17:28:38 +0300

    Тестирование TestDivideStringToChars и TestLateEvaluation
    Финальная версия тестов для функций TestDivideStringToChars и TestLateEvaluation

---
+++ Проект/KNPO_PrIn_266_Nikolaev/TestDivideStringToChars/TestDivideStringToChars.cpp
@@ -0,0 +1,122 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../StringUnpacker/header.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestDivideStringToChars
+{
+	TEST_CLASS(TestDivideStringToChars)
+	{
+	public:
+		
+		TEST_METHOD(SingleCharacter)
+		{
+			// Входные данные
+			std::string inputData = "A";
+
+			// Эталонные выходные данные
+			std::vector<Unit> expOutputData;
+			expOutputData.push_back({1, 'A'});
+
+			// Запуск тестируемой функции
+			std::vector<Unit> outputData = DivideStringToChars(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			for (int i = 0; i < expOutputData.size(); i++)
+			{
+				Assert::AreEqual(expOutputData[i].quantityCurrent, outputData[i].quantityCurrent);
+				Assert::AreEqual(expOutputData[i].Char, outputData[i].Char);
+			}
+		}
+
+		TEST_METHOD(OneRepeatingCharacter)
+		{
+			// Входные данные
+			std::string inputData = "10A";
+
+			// Эталонные выходные данные
+			std::vector<Unit> expOutputData;
+			expOutputData.push_back({ 10, 'A' });
+
+			// Запуск тестируемой функции
+			std::vector<Unit> outputData = DivideStringToChars(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			for (int i = 0; i < expOutputData.size(); i++)
+			{
+				Assert::AreEqual(expOutputData[i].quantityCurrent, outputData[i].quantityCurrent);
+				Assert::AreEqual(expOutputData[i].Char, outputData[i].Char);
+			}
+		}
+
+		TEST_METHOD(MultipleSingleCharacters)
+		{
+			// Входные данные
+			std::string inputData = "ABCD";
+
+			// Эталонные выходные данные
+			std::vector<Unit> expOutputData;
+			expOutputData.push_back({ 1, 'A' });
+			expOutputData.push_back({ 1, 'B' });
+			expOutputData.push_back({ 1, 'C' });
+			expOutputData.push_back({ 1, 'D' });
+
+			// Запуск тестируемой функции
+			std::vector<Unit> outputData = DivideStringToChars(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			for (int i = 0; i < expOutputData.size(); i++)
+			{
+				Assert::AreEqual(expOutputData[i].quantityCurrent, outputData[i].quantityCurrent);
+				Assert::AreEqual(expOutputData[i].Char, outputData[i].Char);
+			}
+		}
+
+		TEST_METHOD(MultipleRepeatingCharacters)
+		{
+			// Входные данные
+			std::string inputData = "10A20B30C40D";
+
+			// Эталонные выходные данные
+			std::vector<Unit> expOutputData;
+			expOutputData.push_back({ 10, 'A' });
+			expOutputData.push_back({ 20, 'B' });
+			expOutputData.push_back({ 30, 'C' });
+			expOutputData.push_back({ 40, 'D' });
+
+			// Запуск тестируемой функции
+			std::vector<Unit> outputData = DivideStringToChars(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			for (int i = 0; i < expOutputData.size(); i++)
+			{
+				Assert::AreEqual(expOutputData[i].quantityCurrent, outputData[i].quantityCurrent);
+				Assert::AreEqual(expOutputData[i].Char, outputData[i].Char);
+			}
+		}
+
+		TEST_METHOD(MixedCharacters)
+		{
+			// Входные данные
+			std::string inputData = "A20BC40D";
+
+			// Эталонные выходные данные
+			std::vector<Unit> expOutputData;
+			expOutputData.push_back({ 1, 'A' });
+			expOutputData.push_back({ 20, 'B' });
+			expOutputData.push_back({ 1, 'C' });
+			expOutputData.push_back({ 40, 'D' });
+
+			// Запуск тестируемой функции
+			std::vector<Unit> outputData = DivideStringToChars(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			for (int i = 0; i < expOutputData.size(); i++)
+			{
+				Assert::AreEqual(expOutputData[i].quantityCurrent, outputData[i].quantityCurrent);
+				Assert::AreEqual(expOutputData[i].Char, outputData[i].Char);
+			}
+		}
+	};
+}
---
+++ Проект/KNPO_PrIn_266_Nikolaev/TestLateEvaluation/TestLateEvaluation.cpp
@@ -0,0 +1,77 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../StringUnpacker/header.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestLateEvaluation
+{
+	TEST_CLASS(TestLateEvaluation)
+	{
+	public:
+		
+		TEST_METHOD(WithoutErrors)
+		{
+			// Входные данные
+			std::string inputData = "A10B";
+
+			// Эталонные выходные данные
+			TermError expOutputData = TermError::Success;
+
+			// Запуск тестируемой функции
+			const auto outputData = LateEvaluation(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			if (expOutputData != outputData) Assert::IsFalse; 
+			else Assert::IsTrue;
+		}
+
+		TEST_METHOD(LastCharacterIsADigit)
+		{
+			// Входные данные
+			std::string inputData = "A10B5";
+
+			// Эталонные выходные данные
+			TermError expOutputData = TermError::EndNumber;
+
+			// Запуск тестируемой функции
+			const auto outputData = LateEvaluation(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			if (expOutputData != outputData) Assert::IsFalse;
+			else Assert::IsTrue;
+		}
+
+		TEST_METHOD(ContainsLowercaseLetters)
+		{
+			// Входные данные
+			std::string inputData = "A10B5ab";
+
+			// Эталонные выходные данные
+			TermError expOutputData = TermError::WrongSymbol;
+
+			// Запуск тестируемой функции
+			const auto outputData = LateEvaluation(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			if (expOutputData != outputData) Assert::IsFalse;
+			else Assert::IsTrue;
+		}
+
+		TEST_METHOD(ContainsNonAlphabeticCharacters)
+		{
+			// Входные данные
+			std::string inputData = "A10B5;.";
+
+			// Эталонные выходные данные
+			TermError expOutputData = TermError::WrongRageOfString;
+
+			// Запуск тестируемой функции
+			const auto outputData = LateEvaluation(inputData);
+
+			// Сравнение полученных выходных данных с эталонными
+			if (expOutputData != outputData) Assert::IsFalse;
+			else Assert::IsTrue;
+		}
+	};
+}
